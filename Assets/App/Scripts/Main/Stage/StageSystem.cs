2bf001dc4dbebb7734b708f9bcd61576883f8fcb
dugite-git <tubasa283one@gmail.com>
2024-08-30 21:52:09 +0900

    Update StageSystem.cs

--- Assets/App/Scripts/Main/Stage/StageSystem.cs
+++ Assets/App/Scripts/Main/Stage/StageSystem.cs
@@ -1,22 +1,33 @@
-namespace App.Main.Stage{
-    public class StageSystem{
-        private readonly int _ballCountonStage  = 0;
+using UnityEngine;
+
+namespace App.Main.Stage
+{
+    public class StageSystem : MonoBehaviour
+    {
+        private readonly int _ballCountonStage = 0;
         public int BallCountonStage => _ballCountonStage;
         ///<summary>
         ///ステージシステム上のボールの数を一つ増やす。
         ///</summary>
-        public IncreaseBallCountonStage(){
+        public IncreaseBallCountonStage()
+        {
             ++_ballCountonStage;
         }
         ///<summary>
         ///ステージシステム上のボールの数を一つ減らす。
         ///</summary>
         ///<exception cref="ArgumentException">ボールの数が0未満になる場合に発生します。</exception>
-        public DecreaseBallCountonStage(){
-            if(_ballCountonStage <= 0){
+        public DecreaseBallCountonStage()
+        {
+            if (_ballCountonStage <= 0)
+            {
                 throw new ArgumentException("Value cannot be negative");
             }
             --_ballCountonStage;
+        }
+
+        void Update()
+        {
         }
     }
 }
