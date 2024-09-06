8b0d6fc2969bd2a2cb5a1d7118c81f37cbc1bf9c
ToYama170402 <100946113+ToYama170402@users.noreply.github.com>
2024-09-07 00:13:34 +0900

    各種ボタンの実装

---
+++ Assets/App/Scripts/Title/GameQuit/QuitButton.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace App.Title.GameQuit
+{
+    public class QuitButton : MonoBehaviour
+    {
+        public void ButtonClick()
+        {
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+
+    }
+}
