f7dfab29290441e2c7454c97d29eda4b2564376e
ryuukitomi <longhuidengmei@gmail.com>
2024-09-06 23:22:21 +0900

    経験値取得の実装
    ItemTableクラス内に経験値を追加する処理を実装した

--- Assets/App/Scripts/Main/Item/ItemTable.cs
+++ Assets/App/Scripts/Main/Item/ItemTable.cs
@@ -9,8 +9,9 @@ namespace App.Main.Item
         public ItemEffect[] items;
 
         void Start()
-        {
-            items = new ItemEffect[2];
+        {   
+            //1～10までは経験値用(仮)
+            items = new ItemEffect[11];
 
             items[0] = new ItemEffect((PlayerDatastore playerDatastore) =>
             {
@@ -18,7 +19,43 @@ namespace App.Main.Item
             });
             items[1] = new ItemEffect((PlayerDatastore playerDatastore) =>
             {
-                Debug.Log("ActivateItem");
+                playerDatastore.AddExperiencePoint(1);//経験値小
+            });
+            items[2] = new ItemEffect((PlayerDatastore playerDatastore) =>
+            {
+                playerDatastore.AddExperiencePoint(3);//経験値中
+            });
+            items[3] = new ItemEffect((PlayerDatastore playerDatastore) =>
+            {
+                playerDatastore.AddExperiencePoint(5);//経験値大
+            });
+            items[4] = new ItemEffect((PlayerDatastore playerDatastore) =>
+            {
+                playerDatastore.AddExperiencePoint(10);//経験値特大
+            });
+            items[5] = new ItemEffect((PlayerDatastore playerDatastore) =>
+            {
+                playerDatastore.AddExperiencePoint(15);//経験値超特大
+            });
+            items[6] = new ItemEffect((PlayerDatastore playerDatastore) =>
+            {
+                Debug.Log("GetExp");
+            });
+            items[7] = new ItemEffect((PlayerDatastore playerDatastore) =>
+            {
+                Debug.Log("GetExp");
+            });
+            items[8] = new ItemEffect((PlayerDatastore playerDatastore) =>
+            {
+                Debug.Log("GetExp");
+            });
+            items[9] = new ItemEffect((PlayerDatastore playerDatastore) =>
+            {
+                Debug.Log("GetExp");
+            });
+            items[10] = new ItemEffect((PlayerDatastore playerDatastore) =>
+            {
+                Debug.Log("GetExp");
             });
         }
 
