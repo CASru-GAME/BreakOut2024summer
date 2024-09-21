395354f24b63bbf025927a2987aa3b8ea67c84b0
dugite-git <tubasa283one@gmail.com>
2024-09-22 00:00:47 +0900

    qaa

--- Assets/App/Scripts/Main/Block/IBlock.cs
+++ Assets/App/Scripts/Main/Block/IBlock.cs
@@ -1,9 +1,11 @@
 using App.Main.Player;
+using App.Main.Stage;
 
 namespace App.Main.Block
 {
     public interface IBlock
     {   
         void TakeDamage(AttackPoint damage);
+        void SetStage(StageSystem stage);
     }
 }
