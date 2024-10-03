18e7b10dde1ef0e1cbd96c712844d54cfb22f870
Motoki4472 <bonjour4472@gmail.com>
2024-10-03 10:31:25 +0900

    バグ修正
    PerkPanelの表示がおかしくなる問題を解決

--- Assets/App/Scripts/Main/Player/Perk/PerkSystem.cs
+++ Assets/App/Scripts/Main/Player/Perk/PerkSystem.cs
@@ -1,8 +1,8 @@
 using App.Main.Player;
 using System;
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
-
 
 namespace App.Main.Player.Perk
 {
@@ -16,62 +16,81 @@ namespace App.Main.Player.Perk
         private GameObject PerkPanel1;
         private GameObject PerkPanel2;
         private GameObject PerkPanel3;
+        private System.Random random = new System.Random(); // Move Random instance to class level
 
-        public PerkSystem(PlayerDatastore playerDatastore , GameObject PerkPanelPrefab)
+        public bool IsPerkChoosing = false;
+
+        public PerkSystem(PlayerDatastore playerDatastore, GameObject PerkPanelPrefab)
         {
             PerkList = new PerkList(playerDatastore);
             AmountPerk = PerkList.AmountPerk;
             this.PerkPanelPrefab = PerkPanelPrefab;
-        }
 
-        private void ChoseRandomPerk()
-        {
-            System.Random random = new System.Random();
-            RandomPerkList.Clear();
-            // AllPerkListからランダムで重複なしでPerkを3つ選択する
-            while(RandomPerkList.Count < 3)
-            {
-                int PerkId = random.Next(0, AmountPerk);
-                if(!RandomPerkList.Contains(PerkId))
-                {
-                    RandomPerkList.Add(PerkId);
-                }
-            }            
-        }
-
-        private void CreatePerkPanel()
-        {
-            // 3つのPerkを画面に表示する
             PerkPanel1 = UnityEngine.Object.Instantiate(
                 PerkPanelPrefab,
                 new Vector3(-5, 0, 0),
                 Quaternion.identity
             ) as GameObject;
-            PerkPanel1.GetComponent<ChoosePerkPanel>().Initialize(RandomPerkList[0], this);
+            PerkPanel1.SetActive(false);
 
             PerkPanel2 = UnityEngine.Object.Instantiate(
                 PerkPanelPrefab,
                 new Vector3(0, 0, 0),
                 Quaternion.identity
             ) as GameObject;
-            PerkPanel2.GetComponent<ChoosePerkPanel>().Initialize(RandomPerkList[1], this);
+            PerkPanel2.SetActive(false);
 
             PerkPanel3 = UnityEngine.Object.Instantiate(
                 PerkPanelPrefab,
                 new Vector3(5, 0, 0),
                 Quaternion.identity
             ) as GameObject;
-            PerkPanel3.GetComponent<ChoosePerkPanel>().Initialize(RandomPerkList[2], this);
-            
+            PerkPanel3.SetActive(false);
         }
+
+        private void ChoseRandomPerk()
+        {
+            RandomPerkList.Clear();
+            // AllPerkListからランダムで重複なしでPerkを3つ選択する
+            while (RandomPerkList.Count < 3)
+            {
+                int PerkId = random.Next(0, AmountPerk);
+                if (!RandomPerkList.Contains(PerkId))
+                {
+                    RandomPerkList.Add(PerkId);
+                }
+            }
+        }
+
+
+
+        private void CreatePerkPanel()
+        {
+            // 3つのPerkを画面に表示する
+            PerkPanel1.GetComponent<ChoosePerkPanel>().Initialize(RandomPerkList[0], this);
+            PerkPanel2.GetComponent<ChoosePerkPanel>().Initialize(RandomPerkList[1], this);
+            PerkPanel3.GetComponent<ChoosePerkPanel>().Initialize(RandomPerkList[2], this);
+            PerkPanel1.SetActive(true);
+            PerkPanel2.SetActive(true);
+            PerkPanel3.SetActive(true);
+        }
+
+
 
         /// <summary>
         /// <para>ランダムでPerkを取得する</para>
         /// </summary>
-        public void ChoosePerk()
+        public IEnumerator ChoosePerk()
         {
+            
+            UnityEngine.Debug.Log("ChoosePerk");
+            IsPerkChoosing = true;
+            UnityEngine.Time.timeScale = 0;
             ChoseRandomPerk();
             CreatePerkPanel();
+            yield return new WaitUntil(() => IsPerkChoosing == false);
+            
+            
         }
 
         public void SusideAll()
