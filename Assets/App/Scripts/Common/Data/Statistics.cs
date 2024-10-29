238afcb409fd6b6df34fe173e393d96866c5912c
dugite-git <tubasa283one@gmail.com>
2024-10-29 23:43:18 +0900

    データ保存のための構造とスクリプトを作成

--- Assets/App/Scripts/Common/Data/Statistics.cs
+++ Assets/App/Scripts/Common/Data/Statistics.cs
@@ -2,25 +2,23 @@ using System.Collections.Generic;
 
 namespace App.Common.Data
 {
-    public class TopValueData
-    {
-        /// 最大レベル
-        public static int _maxPlayerLevel { get; private set; } = -1;
-        /// 最大クリアステージ数
-        public static int _maxClearedStage { get; private set; } = -1;
-        /// 最大救出猫数
-        public static int _maxCat { get; private set; } = -1;
-    }
-
     public class StatisticsData
     {
-        ///プレイした日付
-        public string _playedDate { get; private set; } = "";
+        ///クリアした日付
+        public string _clearedDate { get; private set; } = "";
         /// クリアしたステージ数
         public int _totalClearedStage { get; private set; } = 0;
         /// 助けた猫の数
         public int _totalCat { get; private set; } = 0;
         /// 集めたパーク
         public List<(int id, int stackCount)> _totalAquiredPerkList { get; private set; } = new List<(int id, int stackCount)>();
+
+        public StatisticsData(string clearedDate, int totalClearedStage, int totalCat, List<(int id, int stackCount)> totalAquiredPerkList)
+        {
+            _clearedDate = clearedDate;
+            _totalClearedStage = totalClearedStage;
+            _totalCat = totalCat;
+            _totalAquiredPerkList = totalAquiredPerkList;
+        }
     }
 }
--- Assets/App/Scripts/Common/Data/StatisticsDataController.cs
+++ Assets/App/Scripts/Common/Data/StatisticsDataController.cs
@@ -1,24 +1,29 @@
 using UnityEngine;
 using System.IO;
-using App.Common.Data;
-using UnityEditor.SearchService;
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
+using App.Common.Data.Static;
 
 namespace App.Common.Data
 {
     public class StatisticsDataController : MonoBehaviour
     {
-        public TopValueData topValueData = new TopValueData();
+        /// <summary>
+        /// 統計データリスト
+        /// </summary>
+        /// <remark>
+        /// 降順に並べる
+        /// 日付最新はリストの最後尾
+        /// </remark>
         public List<StatisticsData> statisticsDataList = new List<StatisticsData>();
         public string _currentSceneName = "";
         void Awake()
         {
             _currentSceneName = SceneManager.GetActiveScene().name;
             //JSONファイルの読み込み
-            if (_currentSceneName == "MainScene")
+            if (_currentSceneName == "TitleScene")
             {
-                
+                LoadStatisticsData();
             }
         }
         void OnDestroy()
@@ -26,8 +31,41 @@ namespace App.Common.Data
             //JSONファイルの書き込み
             if (_currentSceneName == "ResultScene")
             {
-
+                //新しいデータを追加
+                StatisticsData newStatisticsData = new StatisticsData(System.DateTime.Now.ToString(), StatisticsDatastore._totalClearedStage, StatisticsDatastore._totalCat, StatisticsDatastore._totalAquiredPerkList);
+                LoadStatisticsData();
+                statisticsDataList.Add(newStatisticsData);
+                //JSONファイルに保存
+                SaveStatisticsData();
             }
         }
+
+        //List<StatisticsData>→JSONの変換
+        public void LoadStatisticsData()
+        {
+            string filePath = Application.persistentDataPath + "/Resources/StatisticsDataList.json";
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                Debug.Log("Loaded : \n" + json);
+                statisticsDataList = JsonUtility.FromJson<StatisticsDataListWrapper>(json).statisticsDataList;
+            }
+        }
+
+        //JSON→List<StatisticsData>の変換
+        public void SaveStatisticsData()
+        {
+            string filePath = Application.persistentDataPath + "/Resources/StatisticsDataList.json";
+            string json = JsonUtility.ToJson(new StatisticsDataListWrapper { statisticsDataList = statisticsDataList });
+            Debug.Log("Saved : \n" + json);
+            Debug.Log(json);
+            File.WriteAllText(filePath, json);
+        }
+    }
+
+    [System.Serializable]
+    public class StatisticsDataListWrapper
+    {
+        public List<StatisticsData> statisticsDataList;
     }
 }
