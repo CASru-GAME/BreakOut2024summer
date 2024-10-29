1b437984d86a05bc4e53aec87ee889899dd65607
dugite-git <tubasa283one@gmail.com>
2024-10-30 01:13:29 +0900

    修正

--- Assets/App/Scripts/Common/Data/StatisticsDataController.cs
+++ Assets/App/Scripts/Common/Data/StatisticsDataController.cs
@@ -33,7 +33,7 @@ namespace App.Common.Data
             if (_currentSceneName == "ResultScene")
             {
                 //新しいデータを追加
-                StatisticsData newStatisticsData = new StatisticsData(System.DateTime.Now.ToString(), StatisticsDatastore._totalClearedStage, StatisticsDatastore._totalCat);
+                StatisticsData newStatisticsData = new StatisticsData(System.DateTime.Now.ToString(), StatisticsDatastore._totalClearedStage, StatisticsDatastore._totalCat, StatisticsDatastore._totalAquiredPerkList);
                 statisticsDataList = new List<StatisticsData>();
                 LoadStatisticsData();
                 statisticsDataList.Add(newStatisticsData);
